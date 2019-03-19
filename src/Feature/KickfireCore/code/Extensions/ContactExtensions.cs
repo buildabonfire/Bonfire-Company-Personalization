c872c8840a3b9eba17f4b43223fc8dcfa8a6ae03
Chris Auer <cja@buildabonfire.com>
2019-03-19 13:46:24 -0400

    Added contact facet extension

---
+++ src/Feature/KickfireCore/code/Extensions/ContactExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Bonfire.Feature.KickfireCore.Models.Facets;
+
+namespace Bonfire.Feature.KickfireCore.Extensions
+{
+    public static class ContactExtensions
+    {
+        public static CompanyFacet Company(this Sitecore.XConnect.Contact c)
+        {
+            return c.GetFacet<CompanyFacet>(CompanyFacet.DefaultFacetKey);
+        }
+    }
+}
