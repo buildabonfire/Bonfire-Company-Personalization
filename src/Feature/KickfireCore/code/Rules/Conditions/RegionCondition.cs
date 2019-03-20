7e79285564e37c33f3f3a45483494298c7caaf5c
Chris Auer <cja@buildabonfire.com>
2019-03-19 20:34:06 -0400

    Added region condition

---
+++ src/Feature/KickfireCore/code/Rules/Conditions/RegionCondition.cs
@@ -0,0 +1,31 @@
+﻿using Bonfire.Feature.KickfireCore.Models.Facets;
+using Sitecore.Analytics;
+using Sitecore.Analytics.XConnect.Facets;
+using Sitecore.Data;
+using Sitecore.Rules;
+using Sitecore.Rules.Conditions;
+
+namespace Bonfire.Feature.KickfireCore.Rules.Conditions
+{
+    public class RegionCondition<T> : WhenCondition<T> where T : RuleContext
+    {
+        public string Region { get; set; }
+
+        protected override bool Execute(T ruleContext)
+        {
+            var xConnectFacet = Tracker.Current.Contact.GetFacet<IXConnectFacets>("XConnectFacets");
+            var company = xConnectFacet.Facets[CompanyFacet.DefaultFacetKey] as CompanyFacet;
+
+            if (company == null) return false;
+
+            return company.Region == GetRegion(Region);
+        }
+
+        private string GetRegion(string id)
+        {
+            if (!ID.IsID(id)) return string.Empty;
+
+            return Sitecore.Context.Database.GetItem(ID.Parse(id))?[Templates.Region.Fields.Description] ?? string.Empty;
+        }
+    }
+}
