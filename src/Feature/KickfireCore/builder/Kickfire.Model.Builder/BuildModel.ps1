53925bb0847f621428c61d3895c1579c8fd2789a
Chris Auer <cja@buildabonfire.com>
2019-04-29 10:32:13 -0400

    Update build PS to restart the rigth services

--- src/Feature/KickfireCore/builder/Kickfire.Model.Builder/BuildModel.ps1
+++ src/Feature/KickfireCore/builder/Kickfire.Model.Builder/BuildModel.ps1
@@ -2,12 +2,12 @@
 Copy-Item ".\CompanyDataModel, 1.0.json" "C:\inetpub\wwwroot\ML.xconnect\App_data\Models\"
 Copy-Item ".\CompanyDataModel, 1.0.json" "C:\inetpub\wwwroot\ML.xconnect\App_data\jobs\continuous\IndexWorker\App_data\Models\"
 
-net stop ML.xconnect-MarketingAutomationService
-net stop ML.xconnect-IndexWorker
+net stop kickfire.xconnect-MarketingAutomationService
+net stop kickfire.xconnect-IndexWorker
 
 Copy-Item ".\bin\debug\Bonfire.Feature.KickfireCore.*"  "C:\inetpub\wwwroot\ML.xconnect\bin\"
 Copy-Item ".\bin\debug\Bonfire.Feature.KickfireCore.*"  "C:\inetpub\wwwroot\ML.xconnect\App_data\jobs\continuous\IndexWorker\"
 Copy-Item ".\bin\debug\Bonfire.Feature.KickfireCore.*"  "C:\inetpub\wwwroot\ML.xconnect\App_data\jobs\continuous\AutomationEngine\"
 
-net start ML.xconnect-MarketingAutomationService
-net start ML.xconnect-IndexWorker
+net start kickfire.xconnect-MarketingAutomationService
+net start kickfire.xconnect-IndexWorker
